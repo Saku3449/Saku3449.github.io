159ae6f5672ce0fa8435650fb64c584d072f4821
saku <saku.studypg@gmail.com>
2022-05-03 01:15:16 +0900

    complete responsive

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 window.addEventListener('DOMContentLoaded', function(){
     window.addEventListener('resize', function(){
+        if(window.outerWidth < 600) {
+            document.querySelector('#access-item').setAttribute('class', '');
+        }
+        else {
+            document.querySelector('#access-item').setAttribute('class', 'd-flex');
+        }
         if(window.outerWidth < 570) {
             document.querySelector('#top-left-item').setAttribute('class', 'item');
             document.querySelector('#top-right-item').setAttribute('class', 'item');
